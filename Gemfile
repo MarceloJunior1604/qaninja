85d4d1c3d3efb7dffc434334d2314affaaeaa365
MarceloJunior1604 <marcelojunior1604@hotmail.com>
2022-05-23 16:48:20 -0300

    Atualização

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source "https://rubygems.org"
 gem 'cucumber', '5.2'
 gem 'capybara', '3.33'
 gem 'selenium-webdriver', '3.142.7'
+gem 'rspec', '3.11'
+gem 'faker', '2.21'
--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,13 +1,20 @@
 Dado('que acesso a página de cadastro') do                                   
     visit "http://rocklov-web:3000/signup"
-    sleep 20
+    
   end                                                                          
                                                                                
   Quando('submeto o meu cadastro completo') do    
+    find("#fullName").set "Marcelo Junior"
+    find("#email").set Faker::Internet.free_email
+    find("#password").set "123456"
+
+    click_button "Cadastrar"
+    
                                  
     
   end                                                                          
                                                                                
-  Então('sou redirecionado para o Dashboard') do                               
-    
+  Então('sou redirecionado para o Dashboard') do     
+    expect(page).to have_css ".dashboard"                          
+    sleep 15
   end                                                                          
