e4ff86d52e0ff89e9c583f6f390c79f7577c6259
MarceloJunior1604 <marcelojunior1604@hotmail.com>
2022-05-23 23:58:34 -0300

    Todos os testes automatizados

--- features/cadastro.feature
+++ features/cadastro.feature
@@ -9,27 +9,27 @@ Funcionalidade: Cadastro
         Dado que acesso a página de cadastro
         Quando submeto o meu cadastro completo
         Então sou redirecionado para o Dashboard
+    @sem_nome
+    Cenario: Submeter cadastro sem o nome
 
-    # Cenario: Submeter cadastro sem o nome
+        Dado que acesso a página de cadastro
+        Quando submeto o meu cadastro sem o nome
+        Então vejo a mensagem de alerta: Oops. Informe seu nome completo!
+    @sem_email
+     Cenario: Submeter cadastro sem o email
 
-    #     Dado que acesso a página de cadastro
-    #     Quando submeto o meu cadastro sem o nome
-    #     Então vejo a mensagem de alerta: Oops. Informe seu nome completo!
+         Dado que acesso a página de cadastro
+         Quando submeto o meu cadastro sem o email
+         Então vejo a mensagem de alerta: Oops. Informe um email válido!
+    @email_incorreto
+     Cenario: Submeter cadastro com email incorreto
 
-    # Cenario: Submeter cadastro sem o email
+         Dado que acesso a página de cadastro
+         Quando submeto o meu cadastro com email incorreto
+         Então vejo a mensagem de alerta: Oops. Informe um email válido!
+    @sem_senha
+     Cenario: Submeter cadastro sem a senha
 
-    #     Dado que acesso a página de cadastro
-    #     Quando submeto o meu cadastro sem o email
-    #     Então vejo a mensagem de alerta: Oops. Informe um email válido!
-
-    # Cenario: Submeter cadastro com email incorreto
-
-    #     Dado que acesso a página de cadastro
-    #     Quando submeto o meu cadastro com incorreto
-    #     Então vejo a mensagem de alerta: Oops. Informe um email válido!
-
-    # Cenario: Submeter cadastro sem a senha
-
-    #     Dado que acesso a página de cadastro
-    #     Quando submeto o meu cadastro sem a senha
-    #     Então vejo a mensagem de alerta: Oops. Informe sua senha secreta!
+         Dado que acesso a página de cadastro
+         Quando submeto o meu cadastro sem a senha
+         Então vejo a mensagem de alerta: Oops. Informe sua senha secreta!
--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -1,20 +1,77 @@
-Dado('que acesso a página de cadastro') do                                   
+
+  #Funcionalidade: Cadastro
+  #Sendo um músico que possui equipamentos musicais
+  #Quero fazer o meu cadastro no RockLov
+  #Para que eu possa disponibilizá-los para locação
+
+  Dado('que acesso a página de cadastro') do                                   
     visit "http://rocklov-web:3000/signup"
     
   end                                                                          
-                                                                               
+  #Cenário -- Fazer cadastro                                                                             
   Quando('submeto o meu cadastro completo') do    
     find("#fullName").set "Marcelo Junior"
     find("#email").set Faker::Internet.free_email
     find("#password").set "123456"
-
-    click_button "Cadastrar"
-    
-                                 
-    
+  click_button "Cadastrar"
   end                                                                          
                                                                                
   Então('sou redirecionado para o Dashboard') do     
     expect(page).to have_css ".dashboard"                          
     sleep 15
   end                                                                          
+
+  #Cenário -- Submeter cadastro sem o nome
+  Quando('submeto o meu cadastro sem o nome') do
+    find("#email").set Faker::Internet.free_email
+    find("#password").set "123456"
+
+    click_button "Cadastrar"
+  end
+  
+  Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do
+     alert = find(".alert-dark")
+     expect(alert.text).to eql "Oops. Informe seu nome completo!"
+  
+  end
+
+  #Cenário -- Submeter cadastro sem o email
+  Quando('submeto o meu cadastro sem o email') do
+    find("#fullName").set "Marcelo Junior"
+    find("#password").set "1234"
+    
+    click_button "Cadastrar"
+  end
+  
+  Então('vejo a mensagem de alerta: Oops. Informe um email válido!') do
+     alert = find(".alert-dark")
+     expect(alert.text).to eql "Oops. Informe um email válido!"
+  end
+
+  #Cenário -- Submeter cadastro com email incorreto
+  Quando('submeto o meu cadastro com email incorreto') do
+    find("#fullName").set "Marcelo Junior"
+    find("#email").set "testeEmailIválido"
+    find("#password").set "1234"
+
+    click_button "Cadastrar"
+  end
+
+  Então('vejo a mensam de alerta: Oops. Informe um email válido!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe um email válido!"
+   end
+   #find("#email").set Faker::Internet.free_email
+  #Cenario -- Submeter cadastro sem a senha
+  Quando('submeto o meu cadastro sem a senha') do
+   find("#fullName").set "Marcelo Junior"
+   find("#email").set Faker::Internet.free_email
+
+   click_button "Cadastrar"
+   sleep 5
+  end
+  
+  Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eql "Oops. Informe sua senha secreta!"
+  end
